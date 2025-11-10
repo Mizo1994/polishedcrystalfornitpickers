16128464ae1efdeb80d00d3f10b98a49d555c045
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-11-10 12:52:34 +0100

    Change Aipom hatch step cycles to fastest

--- data/pokemon/base_stats/aipom.asm
+++ data/pokemon/base_stats/aipom.asm
@@ -5,7 +5,7 @@
 	db 45 ; catch rate
 	db 94 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for AIPOM, RUN_AWAY, PICKUP, SKILL_LINK
 	db GROWTH_FAST ; growth rate
