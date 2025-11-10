b975060c139605b8a6f97af52455cc7499d5392e
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-11-10 13:09:46 +0100

    Update BlueCardMartData with new prize items

--- data/items/buena_prizes.asm
+++ data/items/buena_prizes.asm
@@ -1,12 +1,23 @@
 BlueCardMartData:
-	db 9
-	db ULTRA_BALL,   2
-	db FULL_RESTORE, 2
-	db ELIXIR,       2
-	db MAX_ELIXIR,   3
-	db NUGGET,       3
-	db RARE_CANDY,   4
-	db EVIOLITE,     5
-	db PP_UP,        6
-	db ABILITY_CAP,  7
+	db 20
+	db ODD_SOUVENIR  1
+	db FIRE_STONE    1
+	db WATER_STONE   1
+	db THUNDERSTONE  1
+	db LEAF_STONE    1
+	db ICE_STONE     1
+	db SUN_STONE     1
+	db MOON_STONE    1
+	db DUSK_STONE    1
+	db SHINY_STONE   1
+	db LINKING_CORD  1
+	db EXP_SHARE     1
+	db MAX_ELIXIR,   1
+	db RARE_CANDY,   1
+	db EVIOLITE,     1
+	db PP_MAX,       1
+	db ABILITY_CAP,  1
+	db ABILITYPATCH, 1
+	db LUCKY_EGG     1
+	db LEFTOVERS     1
 	db -1
