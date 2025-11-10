0b301dd190cfa0490aae59f61d6758df630e0d06
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-11-10 13:18:02 +0100

    Update rooftop sale items and prices

--- data/items/rooftop_sale.asm
+++ data/items/rooftop_sale.asm
@@ -1,17 +1,19 @@
 RooftopSaleData1:
-	db 5
-	dbw POKE_BALL,     150
-	dbw GREAT_BALL,    500
-	dbw SUPER_POTION,  500
-	dbw FULL_HEAL,     300
-	dbw REVIVE,       1200
+	dbw 6
+	dbw HP_UP,   3000
+	dbw PROTEIN, 3000
+	dbw IRON,    3000
+	dbw CARBOS,  3000
+	dbw CALCIUM, 3000
+	dbw ZINC,    3000
 	db -1
 
 RooftopSaleData2:
-	db 5
-	dbw HYPER_POTION, 1000
-	dbw FULL_RESTORE, 2000
-	dbw FULL_HEAL,     300
-	dbw ULTRA_BALL,    600
-	dbw PROTEIN,      8000
+	db 6
+	dbw HP_UP,   3000
+	dbw PROTEIN, 3000
+	dbw IRON,    3000
+	dbw CARBOS,  3000
+	dbw CALCIUM, 3000
+	dbw ZINC,    3000
 	db -1
